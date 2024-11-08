03fa60fa2498ef0c62b736c76518b57f952cd161
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-08 23:01:36 +0530

    Number Changing

---
+++ Main.py
@@ -0,0 +1,18 @@
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+z = input("Enter the third number: ")
+
+print("Before swapping:")
+print("First number:", x)
+print("Second number:", y)
+print("Third number:", z)
+
+temp = x
+x = y
+y = z
+z = temp
+
+print("After swapping:")
+print("First number:", x)
+print("Second number:", y)
+print("Third number:", z)
